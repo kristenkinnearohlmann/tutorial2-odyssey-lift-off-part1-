5eb99c5065ab8ead2cf2e9977bbbff150a41455d
Kristen Kinnear-Ohlmann <kristenkinnearohlmann@gmail.com>
2023-04-06 15:29:45 -0500

    Create Track type

--- server/src/schema.js
+++ server/src/schema.js
@@ -1,5 +1,20 @@
 const gql = require("graphql-tag");
 
-const typDefs = gql``;
+const typDefs = gql`
+  "A trackis a group of Modules that teaches about a specific topic"
+  type Track {
+    id: ID!
+    "The teack's title"
+    title: String!
+    "The track's main author"
+    author: Author!
+    "The track's main illustration to display in track card or track page detail"
+    thumbnail: String
+    "The track's approximate length to complete, in minutes"
+    length: Int
+    "The number of modules this track contains"
+    modulesCount: Int
+  }
+`;
 
 module.exports = typDefs;
