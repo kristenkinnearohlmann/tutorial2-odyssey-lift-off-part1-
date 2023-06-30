1552e8aea1d6c449bdb064bcec4f5911f747b164
Kristen Kinnear-Ohlmann <kristenkinnearohlmann@gmail.com>
2023-06-29 19:22:29 -0500

    Draft server implementation

--- server/src/index.js
+++ server/src/index.js
@@ -1,1 +1,6 @@
 //TODO
+const { ApolloServer } = require("@apollo/server");
+const { startStandaloneServer } = require("@apollo/server/standalone");
+const typeDefs = require("./schema");
+
+async function startApolloServer() {}
