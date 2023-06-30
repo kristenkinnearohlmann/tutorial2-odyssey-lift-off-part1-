56ca2a321d332142158350e5aedd2b210b76a683
Kristen Kinnear-Ohlmann <kristenkinnearohlmann@gmail.com>
2023-06-29 19:32:45 -0500

    Implement server items

--- server/src/index.js
+++ server/src/index.js
@@ -1,6 +1,14 @@
-//TODO
 const { ApolloServer } = require("@apollo/server");
 const { startStandaloneServer } = require("@apollo/server/standalone");
 const typeDefs = require("./schema");
 
-async function startApolloServer() {}
+async function startApolloServer() {
+  const server = new ApolloServer({ typeDefs });
+  const { url } = await startStandaloneServer(server);
+  console.log(`
+  Server is running!
+  Query at ${url}
+  `);
+}
+
+startApolloServer();
