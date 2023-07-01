cae259e3a3dd346133531f91e8efae1bee685a8a
Kristen Kinnear-Ohlmann <kristenkinnearohlmann@gmail.com>
2023-06-30 20:48:29 -0500

    Add mock and schema libs, draft mock data

--- server/src/index.js
+++ server/src/index.js
@@ -1,9 +1,21 @@
 const { ApolloServer } = require("@apollo/server");
 const { startStandaloneServer } = require("@apollo/server/standalone");
 const typeDefs = require("./schema");
+const { addMocksToSchema } = require("@graphql-tools/mock");
+const { makeExecutableSchema } = require("@graphql-tools/schema");
+const mocks = {
+  Track: () => ({
+    id: () => "track_01",
+    title: () => "Astro Kitty, Space Explorer",
+  }),
+};
 
 async function startApolloServer() {
-  const server = new ApolloServer({ typeDefs });
+  const server = new ApolloServer({
+    schema: addMocksToSchema({
+      schema: makeExecutableSchema({ typeDefs }),
+    }),
+  });
   const { url } = await startStandaloneServer(server);
   console.log(`
   🚀  Server is running!
