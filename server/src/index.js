7f81ccb1203bd7cae6544fc87849948f8514319c
Kristen Kinnear-Ohlmann <kristenkinnearohlmann@gmail.com>
2023-06-30 20:51:01 -0500

    Complete mock, add to server constructor

--- server/src/index.js
+++ server/src/index.js
@@ -7,6 +7,17 @@ const mocks = {
   Track: () => ({
     id: () => "track_01",
     title: () => "Astro Kitty, Space Explorer",
+    author: () => {
+      return {
+        name: "Grumpy Cat",
+        photo:
+          "https://res.cloudinary.com/dety84pbu/image/upload/v1606816219/kitty-veyron-sm_mctf3c.jpg",
+      };
+    },
+    thumbnail: () =>
+      "https://res.cloudinary.com/dety84pbu/image/upload/v1598465568/nebula_cat_djkt9r.jpg",
+    length: () => 1210,
+    modulesCount: () => 6,
   }),
 };
 
@@ -14,6 +25,7 @@ async function startApolloServer() {
   const server = new ApolloServer({
     schema: addMocksToSchema({
       schema: makeExecutableSchema({ typeDefs }),
+      mocks,
     }),
   });
   const { url } = await startStandaloneServer(server);
